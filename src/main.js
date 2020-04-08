8873d7087cc02c85210a6d6e37cbf7ef93d3ea97
zhibin <zhi233@foxmail.com>
2020-04-08 09:08:26 +0800

    项目的初始化提交

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/element.js'
 
+import axios from 'axios'
+// 挂载axios
+axios.defaults.baseURL = '/manager'
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
